95f6a46bdd4e49b3e08828a381a8637480f9cf3b
Forrest Akin <forrest.akin@cj.com>
2022-07-29 16:26:09 -0700

    if

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,13 +4,21 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
+    }
+}
+
+pub fn foo_if_fizz2(fizzish: &str) -> &str {
+    match fizzish {
+        "fizz" => "foo",
+        "fuzz" => "bar",
+        _ => "baz"
     }
 }
 
